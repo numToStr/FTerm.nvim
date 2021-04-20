d1bad589cf2202ac068607fbaea7058e08539b8d
numtostr <vikasraj1911@gmail.com>
2021-04-20 14:40:48 +0530

    added comment about setting filetype

--- lua/FTerm/terminal.lua
+++ lua/FTerm/terminal.lua
@@ -93,6 +93,9 @@ function Terminal:create_win(buf, opts)
     local win_handle = api.nvim_open_win(buf, true, opts)
 
     api.nvim_win_set_option(win_handle, "winhl", "Normal:Normal")
+
+    -- Setting filetype in `create_win()` instead of `create_buf()` because window options
+    -- such as `winhl`, `winblend` should be available after the window is created.
     api.nvim_buf_set_option(buf, "filetype", "FTerm")
 
     return win_handle
